35d1a861b5661ea0bd017fbd03c7a26d4a9fb3ad
Edward McCarthy <efmccarthy26@gmail.com>
2019-01-13 19:29:23 -0700

    Added data

--- client/src/components/IntegerOperators.js
+++ client/src/components/IntegerOperators.js
@@ -4,7 +4,17 @@ class IntegerOperators extends React.Component {
   render() {
     return (
       <div>
-        INTEGERS!!! OPERATORS
+        <form>
+          <select
+            >
+            <option value="null">Select One</option>
+            <option value="equals">equals</option>
+            <option value="between">between</option>
+            <option value="greater than">greater than</option>
+            <option value="less than">less than</option>
+            <option value="in list">in list</option>
+          </select>
+        </form>
       </div>
     )
   }
--- client/src/components/StringOperators.js
+++ client/src/components/StringOperators.js
@@ -4,7 +4,16 @@ class StringOperators extends React.Component {
   render() {
     return (
       <div>
-        STRING OPERATORS
+        <form>
+          <select
+            >
+            <option value="null">Select One</option>
+            <option value="equals">equals</option>
+            <option value="contains">contains</option>
+            <option value="starts with">starts with</option>
+            <option value="in list">in list</option>
+          </select>
+        </form>
       </div>
     )
   }
