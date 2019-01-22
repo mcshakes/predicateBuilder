b64f729cbe651487be457243848aab292c839418
Edward McCarthy <efmccarthy26@gmail.com>
2019-01-21 18:23:20 -0700

    Updates

---
+++ precache-manifest.394f51c82618929c392d1519e3bd0ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18fb7eea8ff2a9853c3f",
+    "url": "/predicateBuilder/static/css/main.e5248b98.chunk.css"
+  },
+  {
+    "revision": "18fb7eea8ff2a9853c3f",
+    "url": "/predicateBuilder/static/js/main.18fb7eea.chunk.js"
+  },
+  {
+    "revision": "21541f4a7f2579a15b7a",
+    "url": "/predicateBuilder/static/js/1.21541f4a.chunk.js"
+  },
+  {
+    "revision": "6db24bb86881cfa65b97",
+    "url": "/predicateBuilder/static/js/runtime~main.6db24bb8.js"
+  },
+  {
+    "revision": "1edb7f53c7435ea322694df5a199fb8e",
+    "url": "/predicateBuilder/index.html"
+  }
+];
